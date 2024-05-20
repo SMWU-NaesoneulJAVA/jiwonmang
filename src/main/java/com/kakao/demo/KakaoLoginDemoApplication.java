2d7194c3c53a6131b441816a1b5a99823d2dff97
Kwak-Jiwon <jw101102@gmail.com>
2024-05-20 17:13:32 +0900

    first demo

---
+++ src/main/java/com/kakao/demo/KakaoLoginDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kakao.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoLoginDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoLoginDemoApplication.class, args);
+	}
+
+}
