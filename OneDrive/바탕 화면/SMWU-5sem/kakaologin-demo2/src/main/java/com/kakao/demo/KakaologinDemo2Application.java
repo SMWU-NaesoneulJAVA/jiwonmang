92ed67653b5f9c443aded220bedea4b51c671704
Kwak-Jiwon <jw101102@gmail.com>
2024-05-22 22:54:42 +0900

    gradle ver

---
+++ OneDrive/바탕 화면/SMWU-5sem/kakaologin-demo2/src/main/java/com/kakao/demo/KakaologinDemo2Application.java
@@ -0,0 +1,13 @@
+package com.kakao.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaologinDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaologinDemo2Application.class, args);
+	}
+
+}
